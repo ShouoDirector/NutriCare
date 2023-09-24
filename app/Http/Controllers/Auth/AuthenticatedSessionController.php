9ee1279a7d52d6f59a19ff6dc663f2f31b5b196b
ShouoDirector <ivanmilesmvista@gmail,com>
2023-09-24 22:53:22 +0800

    Version 1.008 - Conditions Implemented URLredirect

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,18 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        $url = '';
+        if($request->user()->role === 'admin'){
+            $url = '/admin/dashboard';
+        } elseif ($request->user()->role === 'medical_officer'){
+            $url = '/medical_officer/dashboard';
+        } elseif ($request->user()->role === 'school_nurse'){
+            $url = '/school_nurse/dashboard';
+        } elseif ($request->user()->role === 'class_adviser'){
+            $url = '/class_adviser/dashboard';
+        }
+
+        return redirect()->intended($url);
     }
 
     /**
