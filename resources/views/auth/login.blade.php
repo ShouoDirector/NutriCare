db2e9799d897594dba5a0681f62ad79b5948a62e
ShouoDirector <ivanmilesmvista@gmail,com>
2023-09-25 10:08:46 +0800

    Version 1.011 -  Login Head Efficiency Issue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
   @include('include.core.head')
+</head>
 
   <body>
     <!-- Preloader -->
@@ -13,7 +15,7 @@
         <div class="position-relative z-index-5">
           <div class="row">
             <div class="col-xl-7 col-xxl-8">
-              <a href="index-2.html" class="text-nowrap logo-img d-block px-4 py-9 w-100">
+              <a href="/" class="text-nowrap logo-img d-block px-4 py-9 w-100">
                 <img src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/dark-logo.svg" width="180" alt="">
               </a>
               <div class="d-none d-xl-flex align-items-center justify-content-center" style="height: calc(100vh - 80px);">
---
+++ resources/views/include/core/head_links.blade.php
@@ -0,0 +1,94 @@
+
+    <!--  Title -->
+    <title>NutriCare</title>
+    <!--  Required Meta Tag -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="handheldfriendly" content="true" />
+    <meta name="MobileOptimized" content="width" />
+    <meta name="description" content="nutricare" />
+    <meta name="author" content="Nutricare" />
+    <meta name="keywords" content="nutricare" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+<!-- CORE COMPONENTS-->
+<!-- =================================================== -->
+
+    <!--  Favicon -->
+    <link rel="shortcut icon" type="image/png" href="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/logos/favicon.ico" />
+    <!-- Core Css -->
+    <link id="themeColors" rel="stylesheet" href="{{ asset('dist/css/style.min.css') }}" />
+
+
+<!-- OTHER COMPONENTS-->
+<!-- =================================================== -->
+
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/owl.carousel/dist/assets/owl.carousel.min.css') }}">
+
+    <!-- Prism JS -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/prismjs/themes/prism-okaidia.min.css') }}">
+
+    <!-- NoUI Slider (Rating Slider) -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/nouislider-orxe/distribute/nouislider.min.css') }}">
+
+    <!-- J-Query Raty CSS (Rating) -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/jquery-raty-js/lib/jquery.raty.css') }}">
+
+    <!-- Sweet Alert CSS (Dialog Boxes) -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/sweetalert2/dist/sweetalert2.min.css') }}">
+
+    <!-- Bootstrap CSS (Switch, Touchspin, Date Picker, DateRange Picker, Material DateTime Picker, Table, DataTable, FootTable) -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/bootstrap-table/dist/bootstrap-table.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/footable-v3/compiled/footable.bootstrap.min.css') }}">
+
+    <!-- Dropzone (Drag File/s) -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/dropzone/dist/min/dropzone.min.css') }}">
+
+    <!-- Dual Light Box -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/bootstrap-duallistbox/dist/bootstrap-duallistbox.min.css') }}">
+
+    <!-- CK Editor -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/ckeditor/samples/toolbarconfigurator/lib/codemirror/neo.css') }}">
+
+    <!-- Quill -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/quill/dist/quill.snow.css') }}">
+
+    <!-- SummerNote -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/summernote/dist/summernote-lite.min.css') }}">
+
+    <!-- Cropper (Image Cropper) -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/cropper/dist/cropper.min.css') }}">
+
+    <!-- MiniColors CSS -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/@claviska/jquery-minicolors/jquery.minicolors.css') }}">
+
+    <!-- DateTime Picker -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/pickadate-jalaali/lib/themes/default.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/pickadate-jalaali/lib/themes/default.date.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/libs/pickadate-jalaali/lib/themes/default.time.css') }}">
+
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/select2/dist/css/select2.min.css') }}">
+
+    <!-- Magnific PopUp -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/magnific-popup/dist/magnific-popup.css') }}">
+
+    <!-- Vanilla DataTable -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/vanilla-datatables-editable/datatable.editable.min.css') }}">
+
+    <!-- JSgrid-->
+    <link rel="stylesheet" href="{{ asset('assets/extra-libs/jsgrid/jsgrid.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/extra-libs/jsgrid/jsgrid-theme.min.css') }}">
+
+    <!-- TableSaw -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/tablesaw/dist/tablesaw.css') }}">
+
+    <!-- Dragula - (Draggable Card) -->
+    <link rel="stylesheet" href="{{ asset('dist/libs/dragula/dist/dragula.min.css') }}">
